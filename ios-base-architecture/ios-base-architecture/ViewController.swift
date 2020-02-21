88f25d93daf919a49de1cd2d82e489f809e5bb11
Luiz Hammerli <>
2020-02-21 14:13:32 -0300

    - Adds iOS default project

---
+++ ios-base-architecture/ios-base-architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-base-architecture
+//
+//  Created by Luiz on 21/02/20.
+//  Copyright © 2020 PEBMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
